4fe08816d28479c927d8fa85c46f13aee6b010db
Leo Antunes <antunesleo4@gmail.com>
2024-02-14 19:01:17 -0300

    small adjustments

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from flask import Flask
+from psycopg_pool import ConnectionPool
 
 app = Flask(__name__)
+# pool = ConnectionPool(min_size=2)
+# pool.wait()
 
 @app.route("/")
 def hello_world():
+    # with pool.connection() as conn:
+    #     pass
     return "<p>Hello, World!</p>"
