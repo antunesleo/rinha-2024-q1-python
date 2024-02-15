b637f0469281fe7c7e876706325407d215581d19
Leo Antunes <antunesleo4@gmail.com>
2024-02-15 19:48:00 -0300

    implementa rota de cria transacao

--- gunicorn_conf.py
+++ gunicorn_conf.py
@@ -1,6 +1,6 @@
 import os
 
-max_requests = 500 # totally arbitrary numbers, need adjustments
-max_requests_jitter = 150 # totally arbitrary numbers, need adjustments
-workers = 3 # totally arbitrary numbers, need adjustments
+max_requests = 500  # totally arbitrary numbers, need adjustments
+max_requests_jitter = 150  # totally arbitrary numbers, need adjustments
+workers = 3  # totally arbitrary numbers, need adjustments
 worker_class = "gevent"
--- main.py
+++ main.py
@@ -1,12 +1,40 @@
-from flask import Flask
+import json
+from flask import Flask, request
 from psycopg_pool import ConnectionPool
 
 app = Flask(__name__)
-pool = ConnectionPool("host=localhost port=5432 dbname=rinhapython user=postgres password=postgres", min_size=2)
+pool = ConnectionPool(
+    "host=localhost port=5432 dbname=rinhapython user=postgres password=postgres",
+    min_size=2,
+)
 pool.wait()
+
 
 @app.route("/")
 def hello_world():
+    print(request.data)
     with pool.connection() as conn:
         result = conn.execute("SELECT 1+1").fetchone()
         return f"<p>1 + 1 = {result[0]}</p>"
+
+
+@app.route("/clientes/<cliente_id>/transacoes", methods=["POST"])
+def cria_transacao(cliente_id):
+    payload = json.loads(request.data)
+
+    with pool.connection() as conn:
+        saldo, limite = conn.execute(
+            f"SELECT saldo, limite FROM clientes WHERE id = {cliente_id} FOR UPDATE"
+        ).fetchone()
+        if payload["tipo"] == "c":
+            saldo_futuro = saldo + payload["valor"]
+        else:
+            saldo_futuro = saldo - payload["valor"]
+
+        if saldo_futuro < 0 and abs(saldo_futuro) > limite:
+            return "", 422
+
+        conn.execute(
+            f"INSERT INTO transacoes (cliente_id, tipo, descricao, valor) VALUES ({cliente_id}, '{payload['tipo']}', '{payload['descricao']}', {payload['valor']})"
+        )
+        return "", 200
