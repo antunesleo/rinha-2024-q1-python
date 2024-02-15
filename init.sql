16507ca4cae4c5330b4e2c1a4a59d7ad3519b0c3
Leo Antunes <antunesleo4@gmail.com>
2024-02-15 14:20:11 -0300

    add clientes table and data migration

--- init.sql
+++ init.sql
@@ -1,1 +1,7 @@
-CREATE TABLE TABELINHA ( id serial );
+CREATE TABLE clientes (id SERIAL PRIMARY KEY, limite INT, saldo INT);
+
+INSERT INTO clientes (id, limite, saldo) VALUES (1, 100000, 0);
+INSERT INTO clientes (id, limite, saldo) VALUES (2, 80000, 0);
+INSERT INTO clientes (id, limite, saldo) VALUES (3, 1000000, 0);
+INSERT INTO clientes (id, limite, saldo) VALUES (4, 10000000, 0);
+INSERT INTO clientes (id, limite, saldo) VALUES (5, 500000, 0);
