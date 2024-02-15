88604ca67bd708ebcb94ec51f71a864dc42db10d
Leo Antunes <antunesleo4@gmail.com>
2024-02-14 22:50:45 -0300

    implement connection pool

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from flask import Flask
 from psycopg_pool import ConnectionPool
 
 app = Flask(__name__)
-# pool = ConnectionPool(min_size=2)
-# pool.wait()
+pool = ConnectionPool("host=localhost port=5432 dbname=rinhapython user=postgres password=postgres", min_size=2)
+pool.wait()
 
 @app.route("/")
 def hello_world():
-    # with pool.connection() as conn:
-    #     pass
-    return "<p>Hello, World!</p>"
+    with pool.connection() as conn:
+        result = conn.execute("SELECT 1+1").fetchone()
+        return f"<p>1 + 1 = {result[0]}</p>"
